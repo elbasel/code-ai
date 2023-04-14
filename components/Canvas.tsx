3d6c6bd311ede477c1526f91e594fc1d568c3f1c
elbasel <abdelrahman.elbasel@gmailcom>
2023-04-14 09:30:05 +0200

    Move canvas logic to css

--- components/Canvas.tsx
+++ components/Canvas.tsx
@@ -1,24 +1,12 @@
-import React, { useRef } from "react";
+import React from "react";
 
 type CanvasProps = {
   children: React.ReactNode;
 };
 
 export const Canvas = ({ children }: CanvasProps): React.ReactElement => {
-  const canvasRef: any = useRef({});
-  const handleOnMouseOver = () => {
-    const canvasRefCurrent: HTMLElement = canvasRef.current;
-    const canvasFirstChild = canvasRefCurrent?.children[0];
-    console.log({ canvasFirstChild });
-    if (!canvasFirstChild) return;
-    canvasFirstChild.classList.add("bg-black");
-  };
   return (
-    <div
-      onMouseOver={handleOnMouseOver}
-      ref={canvasRef}
-      className="fixed inset-0 z-30 w-screen min-h-screen p-4 overflow-visible transform canvas scrollbar-hide min-w-max"
-    >
+    <div className="fixed inset-0 z-30 w-screen min-h-screen p-4 overflow-visible transform canvas scrollbar-hide min-w-max">
       {children}
     </div>
   );
